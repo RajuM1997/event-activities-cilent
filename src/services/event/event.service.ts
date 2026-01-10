f073aa3ea319cefab9204115c639acfc0879c119
RajuM1997 <mraju5797@gmail.com>
2026-01-10 23:00:07 +0600

    add event payload

--- src/services/event/event.service.ts
+++ src/services/event/event.service.ts
@@ -3,5 +3,13 @@ export const createEvent = async (
   _currentState: any,
   formData: any
 ): Promise<any> => {
-  const payload = {};
+  const payload = {
+    eventName: formData.get("eventName"),
+    date: formData.get("date"),
+    category: formData.get("category"),
+    location: formData.get("location"),
+    minParticipants: formData.get("minParticipants"),
+    joiningFee: formData.get("joiningFee"),
+    description: formData.get("description"),
+  };
 };
